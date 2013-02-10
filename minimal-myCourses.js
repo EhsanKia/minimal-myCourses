b6e6d140a6f49ba66f6790d4b1d3b42657f65d30
Ehsan Kia <ph0xb01@gmail.com>
2013-02-09 21:19:46 -0500

    Removed collapser
    myCourse has implemented this and therefore my script is no longer necesary.

--- minimal-myCourses.js
+++ minimal-myCourses.js
@@ -3,13 +3,14 @@
 // @namespace   Minimal myCourses
 // @description	Changes the preview pane split in myCourse from horizontal to vertical
 // @include     https://mycourses2.mcgill.ca/*
-// @version     1.3
+// @version     1.4
 // ==/UserScript==
 
 
 // Vertical split
 var locpath     = window.location.pathname;
 var panepath    = "/d2l/lms/discussions/messageLists/frame_right.d2l";
+var homepath    = "/d2l/lp/homepage/home.d2l"; 
 
 if (locpath == panepath){
 	document.body.setAttribute("rows","100%");
@@ -24,21 +25,4 @@ var navBG = document.getElementsByClassName("d_nb_c1")[0];
 if (navTitle && navBG){
 	navTitle.style.paddingTop = "4px";
 	navBG.style.backgroundPositionY = "-11px";
-}
-
-
-// Hides previous courses
-
-var FallCourses = document.getElementById("z_q");
-
-FallCourses.style.height = "30px";
-FallCourses.style.overflow = "hidden";
-
-FallCourses.addEventListener('click',ToggleHide,false);
-
-function ToggleHide(){
-    if (FallCourses.style.height == "auto")
-        FallCourses.style.height = "30px";
-    else
-    	FallCourses.style.height = "auto";
 }
